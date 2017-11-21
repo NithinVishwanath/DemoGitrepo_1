2e860f9ed3432706337525886b2d6d5ef142546a
Vishwanath <nithin.vishwanath@hpe.com>
2017-11-21 22:52:07 +0530

    added search page featureinto the code

--- src/main/java/org/demogit/pagelibrary/LoginPage.java
+++ src/main/java/org/demogit/pagelibrary/LoginPage.java
@@ -1,5 +1,15 @@
 package org.demogit.pagelibrary;
 
 public class LoginPage {
+	
+	public void loginpage (){
+		
+		System.out.println("Login Page");
+	}
+	
+public void testpage (){
+		
+		System.out.println("Test Page");
+	}
 
 }
---
+++ src/main/java/org/demogit/pagelibrary/SearchPage.java
@@ -0,0 +1,10 @@
+package org.demogit.pagelibrary;
+
+public class SearchPage {
+	
+public void searchpage (){
+		
+		System.out.println("Search Page");
+	}
+
+}
